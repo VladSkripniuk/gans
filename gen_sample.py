27a4faf461d51aacd0e2126b072ece401cc9df97
Ubuntu <ubuntu@ip-172-31-31-58.us-east-2.compute.internal>
2018-02-12 16:25:09 +0000

    sample

--- gen_sample.py
+++ gen_sample.py
@@ -9,56 +9,65 @@ import datasets
 
 import gan
 
+import os
+
 from torchvision.utils import save_image
 from torchvision import transforms
 
-netG = mnistnet.LINnet_G(nc=2, nz=100)
+netG = mnistnet.mnistnet_G(nc=1, nz=100, ngf=128)
+netG = mnistnet.LINnet_G(nc=2, nz=100, ngf=64,bias=True)
 
-# netG.load_state_dict(torch.load('LIN_gan500k/gen_100000.pth'))
-netG.load_state_dict(torch.load('LIN_gan500k4880/gen_5000.pth'))
-netG.eval()
 
 opt = gan.Options()
 opt.cuda = True
 opt.nz = (100,1,1)
 opt.batch_size = 64
+opt.checkpoints = [1000, 2000, 5000, 10000, 20000, 40000, 60000, 100000, 200000, 300000, 500000]
 
-gan1 = gan.GAN(netG, None, None, None, opt)
+dirname = 'LIN_gan500k6/'
 
-iterator_fake = gan1.fake_data_generator(opt.batch_size, opt.nz, None)
+for point in opt.checkpoints:
+	# netG.load_state_dict(torch.load('LIN_gan500k/gen_100000.pth'))
+	netG.load_state_dict(torch.load('{}gen_{}.pth'.format(dirname, point)))
+	netG.eval()
 
-a = next(iterator_fake).data.cpu()
+	gan1 = gan.GAN(netG, None, None, None, opt)
 
-b = torch.from_numpy(np.zeros((opt.batch_size, 3, 48, 80))-1)
-# b.zero_()
-b[:,:2,:,:] = a
-b = b / 2.0 + 0.5
-save_image(b, 'sample_fake.jpeg')
+	iterator_fake = gan1.fake_data_generator(opt.batch_size, opt.nz, None)
+
+	a = next(iterator_fake).data.cpu()
+
+	b = torch.from_numpy(np.zeros((opt.batch_size, 3, 48, 80))-1)
+	# b.zero_()
+	b[:,:2,:,:] = a
+	# b=a
+	b = b / 2.0 + 0.5
+	save_image(b, '{}/sample_fake{}.jpeg'.format(dirname, point))
 
 
-opt = gan.Options()
+# opt = gan.Options()
 
-opt.cuda = True
-opt.batch_size = 64
-opt.conditional = False
-opt.n_classes = 10
-opt.nz = (100,1,1)
+# opt.cuda = True
+# opt.batch_size = 64
+# opt.conditional = False
+# opt.n_classes = 10
+# opt.nz = (100,1,1)
 
-data = datasets.LINDataset(protein='Arp3', transform=transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)))
-# print(data.path)
-# print(data[0].max())
-# print(data[0].min())
-# dfsdf
+# data = datasets.LINDataset(protein='Arp3', transform=transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)))
+# # print(data.path)
+# # print(data[0].max())
+# # print(data[0].min())
+# # dfsdf
 
-mydataloader = datasets.MyDataLoader()
-data_iter = mydataloader.return_iterator(DataLoader(data, batch_size=opt.batch_size, shuffle=True, num_workers=1), is_cuda=opt.cuda, conditional=opt.conditional, pictures=True)
+# mydataloader = datasets.MyDataLoader()
+# data_iter = mydataloader.return_iterator(DataLoader(data, batch_size=opt.batch_size, shuffle=True, num_workers=1), is_cuda=opt.cuda, conditional=opt.conditional, pictures=True)
 
-a = next(data_iter).data.cpu()
+# a = next(data_iter).data.cpu()
 
-b = torch.from_numpy(np.zeros((opt.batch_size, 3, 48, 80))-1)
-# b.zero_()
-b[:,:2,:,:] = a
+# b = torch.from_numpy(np.zeros((opt.batch_size, 3, 48, 80))-1)
+# # b.zero_()
+# b[:,:2,:,:] = a
 
-b = b / 2.0 + 0.5
-save_image(b, 'sample_real.jpeg')
+# b = b / 2.0 + 0.5
+# save_image(b, 'sample_real.jpeg')
 
