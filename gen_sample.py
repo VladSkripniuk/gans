cbcf846ee20e318fd3d656b0b49d571948905bb8
Ubuntu <ubuntu@ip-172-31-31-58.us-east-2.compute.internal>
2018-02-27 08:52:23 +0000

    minor

--- gen_sample.py
+++ gen_sample.py
@@ -15,16 +15,20 @@ from torchvision.utils import save_image
 from torchvision import transforms
 
 netG = mnistnet.mnistnet_G(nc=1, nz=100, ngf=128)
-netG = mnistnet.LINnet_G(nc=2, nz=100, ngf=64,bias=True)
+# netG = mnistnet.LINnet_G(nc=2, nz=106, ngf=64,bias=False)
 
 
 opt = gan.Options()
 opt.cuda = True
+opt.n_classes = 6
 opt.nz = (100,1,1)
 opt.batch_size = 64
+opt.conditional = False
 opt.checkpoints = [1000, 2000, 5000, 10000, 20000, 40000, 60000, 100000, 200000, 300000, 500000]
 
-dirname = 'LIN_gan500k6/'
+dirname = 'LIN_wgan500k6/'
+dirname = 'LIN_wgan6cond/'
+dirname = 'SNGAN/'
 
 for point in opt.checkpoints:
 	# netG.load_state_dict(torch.load('LIN_gan500k/gen_100000.pth'))
@@ -35,14 +39,14 @@ for point in opt.checkpoints:
 
 	iterator_fake = gan1.fake_data_generator(opt.batch_size, opt.nz, None)
 
-	a = next(iterator_fake).data.cpu()
+	a = next(iterator_fake)[0].data.cpu()
 
-	b = torch.from_numpy(np.zeros((opt.batch_size, 3, 48, 80))-1)
+	# b = torch.from_numpy(np.zeros((opt.batch_size, 3, 48, 80))-1)
 	# b.zero_()
-	b[:,:2,:,:] = a
+	# b[:,:2,:,:] = a
 	# b=a
-	b = b / 2.0 + 0.5
-	save_image(b, '{}/sample_fake{}.jpeg'.format(dirname, point))
+	# b = b / 2.0 + 0.5
+	save_image(a, '{}/sample_fake{}.jpeg'.format(dirname, point))
 
 
 # opt = gan.Options()
